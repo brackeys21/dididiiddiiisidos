579e43898a80efce521ed7d32186458f202c71f7
brackeys21 <78796487+brackeys21@users.noreply.github.com>
2021-12-07 16:36:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function setup()
+{
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function preload()
+{
+
+}
+
+function draw()
+{
+    
+}
+
+function take_snapshot()
+{
+    save('MyMustacheImage.png');
+}
